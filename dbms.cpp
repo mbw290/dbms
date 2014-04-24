fd4932f58ad0aa0d3db7d4654915f3238b872f73
Matt <anonym0us@localhost.localdomain>
2014-04-24 09:58:56 -0400

    puppy linked list for mod

---
+++ dbms.cpp
@@ -0,0 +1,93 @@
+#include<iostream>
+#include <stdio.h>
+
+using namespace std;
+
+class Puppies
+{
+  public:
+    int Age;
+    int Breed;
+};
+
+//we need a link to point to the puppy and the next item
+//something to hold the list and methods to add to the list and remove etc.
+class Link
+{
+  public:
+   Puppies *Pup;
+   Link *Next;
+};
+
+class LinkedList
+{
+  private:
+    Link *First;
+  public:
+    LinkedList()
+    {
+      First = NULL;
+    }
+
+   void AddPuppy(int A, int B)
+   {
+     Link *newLink = new Link;
+     Puppies *newPup = new Puppies;
+     //Pup pointer is now pointing to our new puppy
+     newLink->Pup=newPup;
+     newPup->Age=A;
+     newPup->Breed=B;
+     newLink->Next=First; //makes next null
+     First=newLink;
+  }
+
+   void AddPuppyEnd(int A,int B)
+   {
+     Link *lastItem=new Link;
+     Link *newNode=new Link;
+     Puppies *newPup = new Puppies;
+     lastItem=First;
+       while (lastItem->Next != NULL)
+       {
+         lastItem=lastItem->Next;
+       }
+       lastItem->Next=newNode;
+       newNode->Pup=newPup;
+       newPup->Age=A;
+       newPup->Breed=B;
+       newNode->Next=NULL; 
+    }
+
+  void RemovePuppy()
+  {
+   delete First->Pup;
+   First=First->Next;
+  }
+
+  void RemoveList(LinkedList LL)
+  {
+     Link *current=new Link;
+     Link *temp = new Link;
+     current=First;
+       while (current != NULL)
+       {
+       temp=current->Next; 
+       delete current->Pup;
+       delete current;
+       current=temp;
+       }
+    delete current->Pup;
+    delete current;
+  }
+  
+  void Display()
+  {
+   Link *current=First;
+   while (current != NULL)
+   {
+     cout << "Age: " << current->Pup->Age<<"\n"<<"Breed: "<<current->Pup->Breed<<"\n";
+     current = current->Next;
+   }
+  }
+};
+
