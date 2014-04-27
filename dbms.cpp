07e9f4c67274b46f4c5d1eb461df64ece73ef8c0
cuso0508 <rmancuso@student.sjcny.edu>
2014-04-27 16:16:11 -0400

    Update dbms.cpp

--- dbms.cpp
+++ dbms.cpp
@@ -23,6 +23,27 @@ class dbTable
 	}
 };
 
+
+struct Field
+{
+    public:
+    string name;
+};
+
+struct intField : Field
+{
+private: int val;
+};
+
+struct stringField : Field
+{
+private: string val;
+};
+
+struct doubleField : Field
+{
+private: double val;
+};
 //we need a link to point to the puppy and the next item
 //something to hold the list and methods to add to the list and remove etc.
 class Link
@@ -98,3 +119,54 @@ class LinkedList
   }
 };
 
+void insert()
+{
+    cout << "Please choose the number that corresponds with your desired field: ";
+    cout << "1. Int field\n2. Double Field\n3. Add String Field";
+    switch(choice)
+    {
+        case 1:
+            //Add Int Field
+            break;
+        case 2:
+            //Add Double Field
+            break;
+        case 3:
+            //Add String Field
+            break;
+        default:
+            cout << "Choice not recognized, Please try again."
+            break;
+    }
+}
+
+
+
+
+int main()
+{
+    int choice;
+    do
+    {
+        cout << "Please choose the number that corresponds with your desired operation: ";
+        cout << "1. Add Table\n2. Drop Table\n3.Display Table\n4.Exit";
+        switch(choice)
+        {
+            case 1:
+                //Add Table
+                break;
+            case 2:
+                //Drop Table
+                break;
+            case 3:
+                //Display Table
+                break;
+            case 4: exit(0);
+                    break;
+            default:
+                cout << "Choice not recognized, Please try again."
+                break;
+        }
+
+    }while(choice != 4)
+}
